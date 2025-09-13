7763cef2e03b7dc61407b9a37dbbeec25560956b
Adrian Faustino <contact.adrianfaustino@gmail.com>
2025-09-13 13:20:38 -0700

    Create folder structure. Update configs. (#1)

---
+++ src/views/Home.tsx
@@ -0,0 +1,12 @@
+import reactLogo from "@/assets/react.svg";
+
+function Home() {
+  return (
+    <div>
+      <h3>Home</h3>
+      <img src={reactLogo} className="logo react" alt="React logo" />
+    </div>
+  );
+}
+
+export default Home;
