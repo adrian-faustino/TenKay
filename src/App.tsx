36a09ca35088ea7536e1b305718e748dbea04e3d
Alec Faustino <alecfaustino@outlook.com>
2025-09-14 12:58:50 -0600

    create basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Skills from "./views/Skills";
 
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <main>
         <Routes>
           <Route path="/home" element={<Home />} />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center w-full p-4 bg-gray-400 sticky top-0">
+      <div className="flex">
+        <Link to="/home">TenKay</Link>
+      </div>
+
+      <div className="flex gap-4">
+        <Link to="/home">Home</Link>
+        <Link to="/skills">Skills</Link>
+      </div>
+    </nav>
+  );
+}
