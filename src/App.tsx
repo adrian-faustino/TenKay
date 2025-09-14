2dd35936a08792758cec370067f3bfd85d03024a
Alec Faustino <alecfaustino@outlook.com>
2025-09-14 13:56:39 -0600

    Wrap app inside Browser Router. Create a basic navbar.  (#5)

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,21 @@ import Home from "./views/Home";
 import Skills from "./views/Skills";
 
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <main>
-      {/* TODO: create app routes */}
-      <Home />
-      <Skills />
-    </main>
+    <BrowserRouter>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/skills" element={<Skills />} />
+        </Routes>
+        {/* TODO: create app routes */}
+      </main>
+    </BrowserRouter>
   );
 }
 
