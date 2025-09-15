ee8da6da9f5238e0447a8d2af1d1f83eb5fe266b
Alec Faustino <alecfaustino@outlook.com>
2025-09-14 20:41:12 -0600

    mobile one column

--- src/components/Profile/SkillSummaryContainer.tsx
+++ src/components/Profile/SkillSummaryContainer.tsx
@@ -7,11 +7,14 @@ const INITIAL_SKILLS: Skill[] = [
   { title: "Hiking", minutesAllTime: 2300 * HOURS },
   { title: "Piano", minutesAllTime: 4500 * HOURS },
   { title: "Coding", minutesAllTime: 910 * HOURS },
+  { title: "Cooking", minutesAllTime: 1200 * HOURS },
+  { title: "Photography", minutesAllTime: 300 * HOURS },
+  { title: "Writing", minutesAllTime: 600 * HOURS },
 ];
 
 export default function SkillSummaryContainer() {
   return (
-    <div className="w-80 mx-auto flex flex-col gap-4">
+    <div className="w-auto mx-auto overflow-y-scroll grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3  gap-4">
       {INITIAL_SKILLS.map((skill) => (
         <SkillSummary skill={skill} key={skill.title} />
       ))}
