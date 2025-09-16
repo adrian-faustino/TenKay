961bf0c4f65d1be3ef23366367d3bd841f7f7566
Alec Faustino <alecfaustino@outlook.com>
2025-09-15 20:02:22 -0600

    move const in skillSummary Container to a mock file

--- src/components/Profile/SkillSummaryContainer.tsx
+++ src/components/Profile/SkillSummaryContainer.tsx
@@ -1,17 +1,5 @@
 import SkillSummary from "./SkillSummary";
-import type { Skill } from "@/types/skill";
-
-// from Skills.tsx
-const HOURS = 60;
-// TODO make a shared constant file for this to also use in Skills.tsx
-const INITIAL_SKILLS: Skill[] = [
-  { title: "Hiking", minutesAllTime: 2300 * HOURS },
-  { title: "Piano", minutesAllTime: 4500 * HOURS },
-  { title: "Coding", minutesAllTime: 910 * HOURS },
-  { title: "Cooking", minutesAllTime: 1200 * HOURS },
-  { title: "Photography", minutesAllTime: 300 * HOURS },
-  { title: "Writing", minutesAllTime: 600 * HOURS },
-];
+import { INITIAL_SKILLS } from "@/mocks/initial_skills";
 
 export default function SkillSummaryContainer() {
   return (
